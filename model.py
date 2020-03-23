36ac8f499b804ee3838aef1050caff309fc246e6
Thi Hien Doan <Doan@andresnoMac-Pro.local>
2020-03-23 18:27:11 +0900

    update model and change data pre-process

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
+'''
+MODEL'S BASIC BLOCK
+'''
 import torchvision.transforms as transforms
 import torchvision.models as models
 from torchvision.utils import save_image
-# from data_utils import TensorDataset
-# data_utils.TensorDataset
 
 from torch.utils.data import DataLoader, TensorDataset
 from torchvision import datasets
@@ -16,7 +17,7 @@ import torch
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.in_channels, self.out_channels = in_channels, out_channels
+        self.in_channels, self.out_channels, self.stride = in_channels, out_channels, stride
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, 
                      stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
@@ -24,6 +25,11 @@ class ResidualBlock(nn.Module):
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, 
                      padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, 
+                     padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+        self.should_apply_shortcut = self.in_channels != self.out_channels or self.stride != 1
         if self.should_apply_shortcut:
             if downsample is not None:
                 self.downsample = downsample 
@@ -40,51 +46,52 @@ class ResidualBlock(nn.Module):
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
         if self.should_apply_shortcut:
             residual = self.downsample(x)
         out += residual
         out = self.relu(out)
         return out
 
-    @property
-    def should_apply_shortcut(self):
-        return self.in_channels != self.out_channels or self.stride != 1
 
 class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(EncoderBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=3, 
+                        stride=2, padding=1, bias=False)
         self.block1 = ResidualBlock(in_channels, in_channels * 2, stride=2)
-        self.block2 = ResidualBlock(in_channels * 2, in_channels * 3, stride=2)
-        self.block3 = ResidualBlock(in_channels * 3, out_channels, stride=2)
-        # self.conv2 = nn.Conv2d(6, out_channels, 3, 1, 1)
-        # self.conv3 = nn.Conv2d(12, 24, 3, 1, 1)
-        # self.conv4 = nn.Conv2d(24, out_channels, 3, 1, 1)
+        self.base1 = ResidualBlock(in_channels * 2, in_channels * 2)
+        self.block2 = ResidualBlock(in_channels * 2, in_channels * 4, stride=2)
+        self.base2 = ResidualBlock(in_channels * 4, in_channels * 4)
+        self.block3 = ResidualBlock(in_channels * 4, out_channels, stride=2)
+        self.base3 = ResidualBlock(out_channels, out_channels)
         
     def forward(self, x):
+        x = F.relu(self.conv(x))
         x = self.block1(x)
+        x = self.base1(x)
         x = self.block2(x)
+        x = self.base2(x)
         x = self.block3(x)
-        # x = F.relu(self.pool(self.conv3(x)))        
-        # x = F.relu(self.pool(self.conv4(x))) 
+        x = self.base3(x)
         return x
 
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DecoderBlock, self).__init__()
-
-        # decoder
-        # self.conv_trans4 = nn.ConvTranspose2d(in_channels, 24, 4, 2, 1)
-        # self.conv_trans3 = nn.ConvTranspose2d(24, out_channels, 4, 2, 1)
         self.conv_trans3 = nn.ConvTranspose2d(in_channels, 12, 4, 2, 1)
         self.conv_trans2 = nn.ConvTranspose2d(12, 6, 4, 2, 1)
         self.conv_trans1 = nn.ConvTranspose2d(6, out_channels, 4, 2, 1)
-        # self.conv_trans2 = nn.ConvTranspose2d(6, 3, 4, 2, 1)
+        self.conv_trans = nn.ConvTranspose2d(out_channels, out_channels, 4, 2, 1)
         
     def forward(self, x):
         # x = F.relu(self.conv_trans4(x))
         x = F.relu(self.conv_trans3(x))
         x = F.relu(self.conv_trans2(x))
-        x = self.conv_trans1(x)
+        x = F.relu(self.conv_trans1(x))
+        x = self.conv_trans(x)
         return x
 
 class MyModel(nn.Module):
@@ -94,16 +101,10 @@ class MyModel(nn.Module):
         #                   padding_mode='zeros')
         super(MyModel, self).__init__()
 
-        # encoder
-        # self.conv = nn.Conv2d(1, )
         self.encoder = EncoderBlock(in_channels=3, out_channels=24)
-        # self.block2 = ResidualBlock(in_channels=6, out_channels=12)
-        # self.block3 = ResidualBlock(in_channels=12, out_channels=24)
         self.decoder = DecoderBlock(in_channels=24, out_channels=3)
 
     def forward(self, x):
         x = self.encoder(x)
-        # x = self.block2(x)
-        # x = self.block3(x)
         x = self.decoder(x)
         return x
--- train_residual.py
+++ train_residual.py
@@ -25,6 +25,9 @@ import torch
 import time
 
 from model import MyModel
+from dataset import UnlabelDataset
+
+start = time.time()
 
 img_size = (1024, 1024)
 # data transform
@@ -35,81 +38,99 @@ data_transforms = {
         transforms.Resize(img_size),
         # transforms.RandomHorizontalFlip(p=1),
         transforms.ToTensor(),
-        transforms.Normalize([0, 0, 0], [255., 255., 255.])
-        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         # transforms.Grayscale(),
         transforms.Resize(1024),
         # transforms.CenterCrop(512),
         transforms.ToTensor(),
-        transforms.Normalize([0, 0, 0], [255., 255., 255.])
-        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
-epochs = 30
-data_dir = 'dataset/train/'
-dataset = datasets.ImageFolder(data_dir,
-                               data_transforms['train'])
-loader = torch.utils.data.DataLoader(dataset, batch_size=8,
-                                             shuffle=True, num_workers=2)
+
+data_dir = 'dataset/train/500'
+# dataset = datasets.ImageFolder(data_dir,
+#                                data_transforms['train'])
+batch_size = 8
+dataset = UnlabelDataset(data_dir, data_transforms['train'])
+loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                             shuffle=True, num_workers=8)
 dataset_sizes = len(dataset)
+end = time.time() - start
+print("Load and crop take {:.0f}m for {} images".format(end//60, dataset_sizes))
 print("Dataset size: ", dataset_sizes)
-print("Number of epochs: ", epochs)
+print("Number of batchs per epoch: ", dataset_sizes//batch_size)
 
 
 '''
 START TRAINING THE MODEL
 '''
-
-start = time.time()
+# release GPU before running
+# del model, data
+torch.cuda.empty_cache()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = MyModel()
+model.to(device)
+
+start = time.time()
 print(str(model))
 
+rae_version = 'v1'
 funcs = {
     'BCE' : nn.BCEWithLogitsLoss(),
     'MSE' : nn.MSELoss(),
     'VGG' : nn.MSELoss(),
     'MAE' : nn.L1Loss(),
 }
-loss_func = 'MSE'
+loss_func = 'BCE'
+lr = 1e-3
+epochs = 100
+
 criterion = funcs[loss_func]
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
-
-# layer_name = 'encoder'
-# model.encoder.register_forward_hook(get_activation(layer_name))
-
+# optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
+optimizer = optim.Adam(model.parameters(), lr=lr)
 loss_hist = []
 
-print ("Start training model with {} images, size {}, in {} epochs, loss function {} ..."\
-       .format(dataset_sizes, img_size, epochs, loss_func))
+print ("Start training model with {} images, size {}, lr = {}, in {} epochs, loss function {}"\
+       .format(dataset_sizes, img_size, lr, epochs, loss_func))
 for epoch in range(epochs):
     print("Epoch {} ...".format(epoch))
     loss_per_epoch = 0
-    for batch_idx, (data, target) in enumerate(loader):
+    epoch_start = time.time()
+#     for batch_idx, (data, target) in enumerate(loader):
+    for batch_idx, (data, filenames) in enumerate(loader):        
+        data = data.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = criterion(output, data)
+#         pred_features = VGG(output)
+#         true_features = VGG(data)
+#         content_loss = MSE(pred_features.relu4_3, true_features.relu4_3)
+#         mse_loss = MSE(output, data)
+#         loss = 0.3 * content_loss + 0.7 * mse_loss
         loss.backward()
         optimizer.step()
         
         loss_per_epoch += loss.item() * data.size(0) 
-        print('Epoch {}, batch idx {}, loss {}'.format(epoch, batch_idx, loss.item()))
+        epoch_end = time.time() - epoch_start
+        epoch_time = "{:.0f}m:{:.0f}s".format(epoch_end//60, epoch_end%60)
+        print('Epoch {}, batch idx {}, loss {}, take {}'.format(epoch, batch_idx, loss.item(), epoch_time))
     loss_hist.append(loss_per_epoch/dataset_sizes)
 end = time.time() - start
-print("Finish in {:.0f}h:{:.0f}m:{:.0f}s".format(end//3600, end//60, end%60))
+print("Finish in {:.0f}h:{:.0f}m:{:.0f}s".format(end//3600, (end//60)%60, end%60))
 
 # save the model 
-torch.save(model.state_dict(), "results/models/encoder_{}_{}_{}.pth".format(img_size, epochs, loss_func))
+model_name = "results/models/rae_{}_{}_{}.pth".format(rae_version, dataset_sizes, epochs)
+torch.save(model.state_dict(), model_name)
 
 # plot loss history
 plt.plot(loss_hist)
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.savefig('results/img/loss_{}_{}.png'.format(img_size, loss_func))
-
+# plt.savefig('results/img/loss_{}_{}_{}.png'.format(rae_version, dataset_sizes, rae_version))
 
 '''
 VISUALIZE SOME RESULTS
@@ -117,31 +138,32 @@ Follow the guide from this post: https://discuss.pytorch.org/t/visualize-feature
 '''
 import math
 
+# save_model = MyModel()
+# sdict = torch.load(model_name)
+# save_model.load_state_dict(sdict)
+
 def normalize_output(img):
     # img = img - img.min()
     # img = img / img.max()
-    img = img * 255.
+#     img = img * 255. + 127.5
     return img
-# data, _ = loader[0]
-# Plot random input/output of model
-vis_data, _ = next(iter(loader))
-vis_output = model(vis_data)
-
-idx = torch.randint(0, vis_output.size(0), ())
 
 
-pred = normalize_output(vis_output[idx, 0])
-img = vis_data[idx, 0]
+# idx = torch.randint(0, output.size(0), ())
+idx = 2
+pred = normalize_output(output[idx, 0])
+img = data[idx, 0]
+img_name = filenames[idx]
 
-# print(img.size())
-fig, axarr = plt.subplots(1, 2)
-axarr[0].imshow(img.detach().numpy(), cmap='gray')
+print(idx)
+fig, axarr = plt.subplots(1, 2, figsize=(20, 20))
+axarr[0].imshow(img.cpu().detach().numpy(), cmap='gray')
 axarr[0].title.set_text('Input')
 axarr[0].axis('off')
-axarr[1].imshow(pred.detach().numpy(), cmap='gray')
+axarr[1].imshow(pred.cpu().detach().numpy(), cmap='gray')
 axarr[1].title.set_text('Output')
 axarr[1].axis('off')
-plt.savefig('results/img/reconstruct_{}_{}.png'.format(img_size, loss_func))
+plt.savefig('results/img/{}_{}_{}_{}_RAEresconstruct.png'.format(img_name, rae_version, dataset_sizes, epochs))
 
 
 # Visualize feature maps
@@ -152,14 +174,13 @@ def get_activation(name):
     return hook
 
 activation = {}
-
 layer_name = 'encoder'
 model.encoder.register_forward_hook(get_activation(layer_name))
 
-img = vis_data[idx]
-pred = model(img.unsqueeze(0))
+img = data[idx]
+pred = model(img.unsqueeze(0).to(device))
 
-act = activation[layer_name].squeeze()
+act = activation[layer_name].squeeze().cpu()
 print("Feature size: ", act.size())
 num_layers = act.size(0) 
 ncols = 6
@@ -173,17 +194,5 @@ for idx in range(num_layers):
     axs[x, y].imshow(act[idx], cmap='gray')
     axs[x, y].axis('off')
     axs[x, y].title.set_text('Layer {}'.format(idx))
-#     idx += 1
 
-# plt.margins(0, 0)
-plt.savefig('results/img/layer_{}_{}_{}.png'.format(layer_name, img_size, loss_func))
-
-
-# # get filters maps
-# from torchvision.utils import make_grid
-
-# kernels = model.encoder['block3'].weight.detach().clone()
-# kernels = kernels - kernels.min()
-# kernels = kernels / kernels.max()
-# img = make_grid(kernels)
-# plt.imshow(img.permute(1, 2, 0))
+plt.savefig('results/img/{}_{}_{}_{}_RAElayer.png'.format(img_name, rae_version, dataset_sizes, epochs))
